8ad1adf1768d3fc1f6b7c93816cd678f4f70d6af
UnqKode <mycodeunq@gmail.com>
2025-03-28 00:08:19 +0530

    Frontend vite changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'  // This specifies where the built files will be output
+  }
 })
